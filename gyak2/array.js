118798b6063d4915268122b85ec800eaa1348d40
barnabas.balazs <barnabas.balazs@enliven.systems>
2023-09-22 09:32:52 +0200

    Gyak 2

--- gyak2/array.js
+++ gyak2/array.js
@@ -27,7 +27,7 @@ let persons = [
         ]
     },
     {
-        name: 'Fullér Levente',
+        name: 'Áfullér Levente',
         age: 25,
         interests: [
             'Futás',
@@ -61,17 +61,53 @@ let persons = [
 
 
 // Legalább 25 évesek
+const atLeast25 = persons.filter((p) => p.age >= 25);
 
 // Nevek listája fordított ABC rendben
+const sortedPersons = persons.map(a => a.name).sort((a, b) => {
+    // if (a - b > 0) {
+    //     return 1;
+    // } else {
+    //     return -1;
+    // }
+    a.localeCompare(b)
+}).reverse();
+// Alternative: b.localeCompare(a) without reverse
 
 // Egy személy aki szeret futni
+const likesRunning = persons.find(p => p.interests.includes('Futás'));
 
 // Az első "Tamás" indexe a tömbben
+const indexOfFirstTamas = persons.findIndex(p => p.name.includes('Tamás'));
 
 // Hobbik egyedi listája
+const uniqueInterests = [...new Set(persons.flatMap((p) => p.interests))];
 
 // Keresztnevek egyedi listája
+const uniqueNames = new Set(
+    persons.flatMap((p) => {
+        const splitName = p.name.split(' ');
+        splitName.shift();
+        return splitName;
+    })
+)
 
 // Legfiatalabb személy
+const youngestPerson = persons.sort((p1, p2) => p1.age - p2.age)[0];
 
 // Átlagos hobbiszám
+const averageInterestNumber = persons
+    .map(p => p.interests.length)
+    .reduce((partialResult, currentInterestNumber) => partialResult + currentInterestNumber)
+    / persons.length;
+
+console.log(
+    `Legalább 25 évesek:`,  atLeast25,
+    'Rendezett személyes:', sortedPersons,
+    'Szeret futni: ', likesRunning,
+    'Index of first Tamás', persons, indexOfFirstTamas,
+    'Unique interests', uniqueInterests,
+    'Unique names', uniqueNames,
+    'Youngest', youngestPerson,
+    'Average interest number', averageInterestNumber
+);
--- gyak2/sqrt.js
+++ gyak2/sqrt.js
@@ -1,7 +1,31 @@
 // 1. Tervezés
 // 2. DOM
 // 3. Számolás / segédfüggvény
+function calculateSqrt(n) {
+    return Math.sqrt(n);
+}
+
 // 4. Eseménykezelés
-// 5. Validáció
-// 6. Stílus
+const buttonEl = document.querySelector('#submit');
+const inputEl = document.querySelector('#numberInput');
+const resultEl = document.querySelector('#result');
+
+buttonEl.addEventListener('click', (event) => {
+    console.log(event);
+    const number = +inputEl.value;
+    console.log(number);
+    // 5. Validáció
+    if (!isNaN(number) && number > 0) {
+        resultEl.innerHTML = `<p>~*-${calculateSqrt(number)}-*~`;
+        inputEl.style.border = '1px solid black';
+    } else {
+        resultEl.innerHTML = '';
+        // 6. Stílus
+        inputEl.style.border = '1px solid red';
+
+        // inputEl.classList.add('error');
+    }
+})
+
+
 
