75a9bf28a4d2f9dcaf764190feb63d2eab642789
barnabas.balazs <barnabas.balazs@enliven.systems>
2023-10-12 17:55:53 +0200

    Gyak 5 starter kit

---
+++ gyak5/adversary.js
@@ -0,0 +1,29 @@
+// Delegation
+function delegate(parent, type, selector, handler) {
+    parent.addEventListener(type, function (event) {
+        const targetElement = event.target.closest(selector);
+
+        if (this.contains(targetElement)) {
+            handler.call(targetElement, event);
+        }
+    });
+}
+
+function randomNumber(from, to) {
+    return Math.floor(Math.random() * to) + from;
+}
+
+// Init
+
+function adversaryGuess() {
+
+}
+
+function cellAvailable(cell) {
+
+}
+
+function createGrid(size) {
+
+}
+
---
+++ gyak5/form.js
@@ -0,0 +1,7 @@
+// Form
+
+const formEl = document.querySelector('#form');
+const availableInputEl = document.querySelector('#availableInCinemas');
+const nextInputEl = document.querySelector('#next');
+const protagonistInputEl = document.querySelector('#protagonist');
+const titleInputEl = document.querySelector('#title');
