7f0243ce91e7a0b19e2b249af28883cd1e18383c
barnabas.balazs <barnabas.balazs@enliven.systems>
2023-10-20 08:18:14 +0200

    Gyak 6 [WIP]

--- gyak6/gyak6.js
+++ gyak6/gyak6.js
@@ -13,3 +13,26 @@ class Column {
 
 const canvas = document.getElementById('flappyCanvas');
 const context = canvas.getContext('2d');
+
+let previousUpdate = performance.now();
+function cycle(now = performance.now()) {
+    // console.log('cycle');
+    const dt = now - previousUpdate;
+    previousUpdate = now;
+
+    update(dt);
+    draw();
+
+    requestAnimationFrame(cycle);
+}
+
+function draw() {
+    context.fillStyle = 'lightblue';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function update(dt) {
+
+}
+
+cycle();
