8aa9ad7be275b7c71db0830a5e5d3f5bac923cff
barnabas.balazs <barnabas.balazs@enliven.systems>
2023-10-27 06:27:00 +0200

    Gyak 7 starter kit

---
+++ gyak7/index.php
@@ -0,0 +1,8 @@
+<!--
+PHP install:
+https://github.com/totadavid95/PhpComposerInstaller
+OR
+https://www.apachefriends.org/download.html
+
+PHP documentation: https://www.php.net/manual/en/index.php
+-->
